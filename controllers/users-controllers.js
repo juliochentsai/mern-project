4d10153d7cc689024fb93fbd1ec70451fdc0c108
Julio Chen <julio111777@hotmail.com>
2024-01-16 22:36:07 -0500

    Initial Commit

---
+++ controllers/users-controllers.js
@@ -0,0 +1,140 @@
+const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const HttpError = require("../models/http-error");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+const getUsers = async (req, res, next) => {
+  let users;
+  try {
+    users = await User.find({}, "-password");
+  } catch (err) {
+    const error = new HttpError(
+      "Fetching users failed, please try again later",
+      500
+    );
+    return next(error);
+  }
+
+  res.json({ users: users.map((x) => x.toObject({ getters: true })) });
+};
+const signup = async (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    return next(
+      new HttpError("Invalid Input passed please check your data", 422)
+    );
+  }
+  const { name, email, password } = req.body;
+
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError("Sign up failed, please try again later", 500);
+    return next(error);
+  }
+
+  if (existingUser) {
+    const error = new HttpError(
+      "User exist already, please login instead",
+      422
+    );
+    return next(error);
+  }
+
+  let hashedPassword;
+  try {
+    hashedPassword = await bcrypt.hash(password, 12);
+  } catch (err) {
+    const error = new HttpError("Could not create use, could no hash", 500);
+    return next(error);
+  }
+
+  const createdUser = new User({
+    name: name,
+    email: email,
+    image: req.file.path,
+    password: hashedPassword,
+    places: [],
+  });
+
+  try {
+    await createdUser.save();
+  } catch (err) {
+    const error = new HttpError("Signing up failed, please try again.", 500);
+    return next(error);
+  }
+
+  let token;
+  try {
+    token = jwt.sign(
+      { userId: createdUser.id, email: createdUser.email },
+      process.env.JWT_KEY,
+      { expiresIn: "1h" }
+    );
+  } catch (err) {
+    const error = new HttpError("Signing up failed, please try again.", 500);
+    return next(error);
+  }
+
+  res
+    .status(201)
+    .json({ userId: createdUser.id, email: createdUser.email, token: token });
+};
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError(
+      "Logging in failed, please try again later",
+      500
+    );
+    return next(error);
+  }
+
+  if (!existingUser) {
+    const error = new HttpError("Invalid creadentials could not log in", 403);
+    return next(error);
+  }
+
+  let isValidPassword;
+  try {
+    isValidPassword = await bcrypt.compare(password, existingUser.password);
+  } catch (err) {
+    const error = new HttpError(
+      "Could not log in, comparingon hash went wrong",
+      500
+    );
+    return next(error);
+  }
+  if (!isValidPassword) {
+    const error = new HttpError("Credentials error", 403);
+    return next(error);
+  }
+  let token;
+  try {
+    token = jwt.sign(
+      { userId: existingUser.id, email: existingUser.email },
+      process.env.JWT_KEY,
+      { expiresIn: "1h" }
+    );
+  } catch (err) {
+    const error = new HttpError("Logiing in failed, please try again.", 500);
+    return next(error);
+  }
+
+  res.json({
+    userId: existingUser.id,
+    email: existingUser.email,
+    token: token,
+  });
+};
+
+exports.getUsers = getUsers;
+exports.signup = signup;
+exports.login = login;
---
+++ util/location.js
@@ -0,0 +1,7 @@
+const API_KEY = process.env.GOOGLE_API_KEY;
+
+function getCoordsForAddress(address) {
+  return { lat: 123, lng: 456 };
+}
+
+module.exports = getCoordsForAddress;
